923a232efa05dbde6fc2e38df270f73a7ae3ae3a
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-08-09 14:45:34 -0300

    task complete (without functions, just form)

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Image, TouchableOpacity, Text } from 'react-native';
+import { StyleSheet, View, Image, TouchableOpacity, Text, TextInput } from 'react-native';
 import { Video, ResizeMode } from 'expo-av';
 import signal from './assets/batLogo.png';
 import bgVideo from './assets/animatedBg.mp4';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [pressed, setPressed] = useState(false)
+
+  function handlePressedBtn() {
+    setPressed(!pressed)
+  }
+
   return (
     <View style={styles.container}>
       <Video
@@ -15,19 +23,51 @@ export default function App() {
         shouldPlay
         isMuted
       />
-        <View style={styles.containerTop}>
+      <View style={[styles.containerTop, {display: pressed? 'none' : 'flex'}]}>
+        <Image
+          source={signal}
+          style={styles.imageLogo}
+        />
+      </View>
+
+      <TouchableOpacity
+        onPress={handlePressedBtn}>
+        <Text style={[styles.btnText, {display: pressed? 'none' : 'flex'}]}>
+          Ativar Bat-Sinal
+        </Text>
+      </TouchableOpacity>
+
+      <View style={[styles.formContainer, { display: pressed ? 'flex' : 'none' }]}>
+        <Text style={styles.formFont}>Contato: </Text>
+        <TextInput
+          style={styles.inputStyle}
+          placeholder='Nome Completo'
+          placeholderTextColor={'#c0c0c0ff'}
+        />
+        <TextInput
+          style={styles.inputStyle}
+          placeholder='Localização atual'
+          placeholderTextColor={'#c0c0c0ff'}
+        />
+        <Text style={styles.formFont}>Mais informações</Text>
+        <TextInput
+          style={[styles.inputStyle, {height: 200}]}
+          placeholder='Observações gerais...'
+          placeholderTextColor={'#c0c0c0ff'}
+        />
+        <TouchableOpacity
+          style={styles.btnSend}
+          onPress={handlePressedBtn}>
           <Image
             source={signal}
-            style={styles.imageLogo}
+            style={styles.smallLogo}
           />
-        </View>
-
-        <TouchableOpacity>
-          <Text style={styles.btnText}>
-            Ativar Bat-Sinal
-          </Text>
+          <Text
+            style={styles.sendFont}
+          >Enviar!</Text>
         </TouchableOpacity>
-        <StatusBar style="light" />
+      </View>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -44,14 +84,49 @@ const styles = StyleSheet.create({
     flex: 0.5
   },
   imageLogo: {
-    height: 120,
-    width: 190
+    height: 180,
+    width: 240
+  },
+  smallLogo: {
+    height: 40,
+    width: 80
   },
   btnText: {
-    color: 'white',
-    backgroundColor: '#000',
+    color: 'black',
+    backgroundColor: '#ffffffd2',
     fontSize: 24,
+    fontWeight: 'bold',
     padding: 18,
     borderRadius: 18
+  },
+  formContainer: {
+    width: '90%',
+    backgroundColor: '#000000d3',
+    padding: 20,
+    borderRadius: 14
+  },
+  formFont: {
+    color: '#fff',
+    fontSize: 16,
+    paddingVertical: 14
+  },
+  inputStyle: {
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 4,
+    marginVertical: 10
+  },
+  btnSend: {
+    alignItems: 'center',
+    backgroundColor: 'black',
+    borderRadius: 12,
+    borderColor: 'white',
+    borderWidth: 1
+  },
+  sendFont: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold'
   }
 });
